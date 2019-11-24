b7859e43fa6c6af637e3908aa5a5fbf541654aac
mtn <mkaraer44@hotmail.com>
2019-11-24 07:54:36 -0500

    degisiklik

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,10 @@
 public class Calculator {
-    public static double add(double num1, double num2){
-        return num1+num2;
+    public static double add(double num1, double num2) {
+        return num1 + num2;
     }
-    public static double subtract(double num1,double num2){
-        return num1-num2;
+
+
+    public static double subtract(double num1,double num2) {
+        return num1 - num2;
     }
 }
