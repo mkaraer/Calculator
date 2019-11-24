eeebb17a64d4d34fa1356aaa662ee1fb49200f35
mtn <mkaraer44@hotmail.com>
2019-11-24 08:03:03 -0500

    somthing new

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,6 +13,9 @@ public class Calculator {
     public static double divide(double num1,double num2){
     return num1/num2;
     }
+    public static double someting(double num1,double num2){
+        return num1*num2;
+    }
     
     
 }
