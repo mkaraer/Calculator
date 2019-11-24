e04a69c7da5f5c15cbbb7eb31be8234c01f98542
mtn <mkaraer44@hotmail.com>
2019-11-24 08:28:52 -0500

    commit from local

--- src/Calculator.java
+++ src/Calculator.java
@@ -16,6 +16,13 @@ public class Calculator {
     public static double someting(double num1,double num2){
         return num1*num2;
     }
+    public static void salary(int num1,int num2){
+        int salary=num1*num2;
+    }
+    //commit from local repository
+    public static double divided(double num1,double num2){
+        return num1/num2;
+    }
     
     
 }
